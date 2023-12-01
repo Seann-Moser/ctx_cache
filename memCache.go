ff21a7caade2ef618a1ef113e5056c53b19ebfb3
Seann-Moser <smoser@kochava.com>
2023-12-01 08:51:28 -0800

    fix: add metrics; lint errors

--- memCache.go
+++ memCache.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"time"
+
 	"github.com/orijtech/gomemcache/memcache"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
-	"time"
 )
 
 var _ Cache = &MemCache{}
@@ -15,6 +16,7 @@ var _ Cache = &MemCache{}
 type MemCache struct {
 	memcacheClient  *memcache.Client
 	defaultDuration time.Duration
+	cacheTags       CacheTags
 }
 
 func MemcacheFlags(prefix string) *pflag.FlagSet {
@@ -24,13 +26,14 @@ func MemcacheFlags(prefix string) *pflag.FlagSet {
 	return fs
 }
 func NewMemcacheFromFlags(prefix string) *MemCache {
-	return NewMemcache(memcache.New(viper.GetStringSlice(prefix+"memcache-addrs")...), viper.GetDuration(prefix+"memcache-default-duration"))
+	return NewMemcache(memcache.New(viper.GetStringSlice(prefix+"memcache-addrs")...), viper.GetDuration(prefix+"memcache-default-duration"), prefix)
 }
 
-func NewMemcache(cacher *memcache.Client, defaultDuration time.Duration) *MemCache {
+func NewMemcache(cacher *memcache.Client, defaultDuration time.Duration, instance string) *MemCache {
 	return &MemCache{
 		memcacheClient:  cacher,
 		defaultDuration: defaultDuration,
+		cacheTags:       NewCacheTags("memcache", instance),
 	}
 }
 
@@ -43,30 +46,54 @@ func (c *MemCache) Ping(ctx context.Context) error {
 }
 
 func (c *MemCache) SetCache(ctx context.Context, key string, item interface{}) error {
-	if c == nil {
-		return ErrCacheMiss
-	}
+	var cacheErr error
+	s := c.cacheTags.record(ctx, CacheCmdSET, func(err error) CacheStatus {
+		if errors.Is(err, ErrCacheMiss) {
+			return CacheStatusMISSING
+		}
+		if err != nil {
+			return CacheStatusERR
+		}
+		return CacheStatusOK
+	})
+	defer func() {
+		s(cacheErr)
+	}()
 	data, err := json.Marshal(item)
 	if err != nil {
+		cacheErr = err
 		return err
 	}
-
-	return c.memcacheClient.Set(ctx, &memcache.Item{
+	cacheErr = c.memcacheClient.Set(ctx, &memcache.Item{
 		Key:        key,
 		Value:      data,
 		Expiration: int32(c.defaultDuration.Seconds()),
 	})
+	return cacheErr
 }
 
 func (c *MemCache) GetCache(ctx context.Context, key string) ([]byte, error) {
-	if c == nil {
-		return nil, ErrCacheMiss
-	}
+	var cacheErr error
+	s := c.cacheTags.record(ctx, CacheCmdGET, func(err error) CacheStatus {
+		if errors.Is(err, ErrCacheMiss) {
+			return CacheStatusMISSING
+		}
+		if err != nil {
+			return CacheStatusERR
+		}
+		return CacheStatusFOUND
+	})
+	defer func() {
+		s(cacheErr)
+	}()
+
 	it, err := c.memcacheClient.Get(ctx, key)
 	if errors.Is(err, memcache.ErrCacheMiss) {
+		cacheErr = ErrCacheMiss
 		return nil, ErrCacheMiss
 	}
 	if err != nil {
+		cacheErr = err
 		return nil, err
 	}
 	return it.Value, nil

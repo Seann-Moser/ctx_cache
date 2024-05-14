cbcc31b269af008e5227e50159739af8dc8b6440
Seann-Moser <seannmoser@gmail.com>
2024-05-14 11:09:41 -0700

    fix: cache key

--- cache.go
+++ cache.go
@@ -2,6 +2,8 @@ package ctx_cache
 
 import (
 	"context"
+	"crypto/md5"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -60,9 +62,15 @@ func getType(myVar interface{}) string {
 		return t.String()
 	}
 }
+
+func GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return base64.StdEncoding.EncodeToString(hash[:])
+}
+
 func GetKey[T any](key ...string) string {
 	var d T
-	return fmt.Sprintf("%s_%s", getType(d), strings.Join(key, "_"))
+	return GetMD5Hash(fmt.Sprintf("%s_%s", getType(d), strings.Join(key, "_")))
 }
 
 func Set[T any](ctx context.Context, group, key string, data T) error {
@@ -152,7 +160,7 @@ func GetSetP[T any](ctx context.Context, cacheTimeout time.Duration, group, key 
 		if err != nil {
 			return nil, err
 		}
-		return nv, SetWithExpiration[T](ctx, cacheTimeout, group, key, nv)
+		return nv, SetWithExpiration[T](ctx, cacheTimeout, group, key, *nv)
 	} else {
 		return v, nil
 	}

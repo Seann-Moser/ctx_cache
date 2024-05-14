f550e3da0956121bfae2fa64b5f0931d068606c9
Seann-Moser <seannmoser@gmail.com>
2024-05-14 10:17:19 -0700

    fix: cache get/set

---
+++ monitor_test.go
@@ -0,0 +1,76 @@
+package ctx_cache
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+)
+
+type CacheTestMonitor struct {
+	Key            string
+	Group          string
+	Func           func(ctx context.Context) (string, error)
+	Expected       string
+	IsExpectingErr bool
+}
+
+func TestMonitor(t *testing.T) {
+	ctx := context.Background()
+	GlobalCacheMonitor = NewMonitor()
+	workers := 20
+	cacheFunctions := []*CacheTestMonitor{
+		{
+			Key:      "default1",
+			Group:    "default",
+			Expected: time.Now().Add(time.Hour).String(),
+		},
+		{
+			Key:      "default2",
+			Group:    "default",
+			Expected: time.Now().Add(time.Minute).String(),
+		},
+		{
+			Key:      "default3",
+			Group:    "default",
+			Expected: time.Now().Add(time.Minute).String(),
+		},
+		{
+			Key:            "default1",
+			Group:          "default",
+			Expected:       time.Now().Add(time.Hour).String(),
+			IsExpectingErr: true,
+		},
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(workers)
+	for i := 0; i < workers; i++ {
+		go func() {
+			defer wg.Done()
+			for _, cacheFunction := range cacheFunctions {
+				e, err := GetSet[string](ctx, 1*time.Minute, cacheFunction.Group, cacheFunction.Key, NewD(cacheFunction))
+				if err != nil {
+					t.Errorf("failed getting cache %s %s Expected:%s", cacheFunction.Group, cacheFunction.Key, cacheFunction.Expected)
+				}
+				if e != cacheFunction.Expected {
+					if !cacheFunction.IsExpectingErr {
+						t.Errorf("failed getting cache %s %s Actual:%s Expected:%s", cacheFunction.Group, cacheFunction.Key, e, cacheFunction.Expected)
+					}
+				} else if cacheFunction.IsExpectingErr {
+					t.Errorf("should have failed failed getting cache %s %s Actual:%s", cacheFunction.Group, cacheFunction.Key, e)
+				}
+
+			}
+
+		}()
+	}
+
+	wg.Wait()
+}
+
+func NewD(c *CacheTestMonitor) func(ctx context.Context) (string, error) {
+	return func(ctx context.Context) (string, error) {
+		return c.Expected, nil
+	}
+}
